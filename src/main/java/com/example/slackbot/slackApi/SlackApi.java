49687a8e2c78eccf98d111af88dc5543a780a0db
ludwings <ludwings0330@gmail.com>
2022-02-26 18:30:33 +0900

    feat: Slack api postMessage 기능 개발
    - 메세지를 레오폴드 공지사항 채널에 update

---
+++ src/main/java/com/example/slackbot/slackApi/SlackApi.java
@@ -0,0 +1,19 @@
+package com.example.slackbot.slackApi;
+
+import org.springframework.beans.factory.annotation.Value;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+
+public interface SlackApi {
+
+    @Headers({
+            "Content-type: application/json"
+    })
+    @POST("api/chat.postMessage")
+    Call<SlackApiDto.SlackPostMessageResponse> postLeopoldNotice(@Body SlackApiDto.SlackPostMessageRequest request, @Header("Authorization") String authHeader);
+
+}
